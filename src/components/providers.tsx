6c2cfe815dd16faabc3151275d6c2d09d0e5fd91
Luis Llanes <luisllaboj@gmail.com>
2024-11-01 23:45:07 -0700

    fix: remove context

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,10 +1,5 @@
-import { PlayerProvider } from "./context/player-provider";
 import { ThemeProvider } from "./theme-provider";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <ThemeProvider>
-      <PlayerProvider>{children}</PlayerProvider>
-    </ThemeProvider>
-  );
+  return <ThemeProvider>{children}</ThemeProvider>;
 }
